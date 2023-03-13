7cc3bb27fd2959ea4229432b04bae1c25ae41d23
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-03-13 10:41:01 -0300

    install express

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// resp -> mandar resposta pra usuário
 const server = http.createServer((req, resp)=> {
     resp.statusCode = 200;
     resp.setHeader('Content-Type', 'text/plain');
+
+    // resposta para user
     resp.end('Hello World');
 })
 
+// padrão -> porta, host(default: localhost)
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/music_streming/index.js`)
 })
