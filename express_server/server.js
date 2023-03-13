4cfd144ea68d5e910afc776c5487cf54af46e26e
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-03-13 11:18:29 -0300

    basico express concluido

--- express_server/server.js
+++ express_server/server.js
@@ -6,8 +6,13 @@ const app = express();
 // Pasta da req e function(dados da requisicao, resposta)
 app.get("/", (req, res) => {
     res.send("Seja bem vindo")
-    // return res.sendFile(__dirname + '/index.html')
+    
 });
+
+// retornando uma pasta html
+app.get("/pagehmtl", (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
 
 app.get("/param/:param/:param2", (req, res) => {
     res.send(req.params.param + " " + req.params.param2)
